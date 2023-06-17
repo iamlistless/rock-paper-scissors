5c3095c20e5a3d35dbd6be4278cd5ec5c3ae1539
iamlistless <98135875+iamlistless@users.noreply.github.com>
2023-06-17 10:38:19 -0700

    Create function playRound first attempt

--- javascript.js
+++ javascript.js
@@ -1,7 +1,28 @@
-let weapon = ["Rock", "Paper", "Scissors"];
+let computerWeapon = ["Rock", "Paper", "Scissors"];
+let playerWeapon = prompt("Choose your weapon... Rock, Paper, Scissors?")
 
-function getComputerChoice(choiceOfWeapon) { {
-        return choiceOfWeapon[Math.floor(Math.random() * weapon.length)];
-        console.log(getComputerChoice(weapon));
-    }
+function getComputerChoice(choiceOfWeapon) { 
+        return choiceOfWeapon[Math.floor(Math.random() * computerWeapon.length)];
 }
+
+function playRound(playerWeapon, computerWeapon) {
+    playerWeapon.toLowerCase()
+    computerWeapon.toLowerCase()
+    
+    if (playerWeapon == computerWeapon) {
+        return "Draw";
+    } else if (playerWeapon == "rock" && computerWeapon == "paper") {
+        return "Better luck next time."
+    } else if (playerWeapon == "rock" && computerWeapon == "scissors") {
+        return "You win."
+    } else if (playerWeapon == "scissors" && computerWeapon == "rock") {
+        return "Better luck next time."
+    } else if (playerWeapon == "scissors" && computerWeapon == "paper") {
+        return "You win."
+    } else if (playerWeapon == "paper" && computerWeapon == "scissors") {
+        return "Better luck next time."
+    } else if (playerWeapon == "paper" && computerWeapon == "rock") {
+        return "You win."
+    } 
+}
+
