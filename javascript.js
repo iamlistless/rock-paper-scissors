d1a5bc208dc16b94863a48ef0f80c1b220b3b9b8
iamlistless <98135875+iamlistless@users.noreply.github.com>
2023-07-23 14:22:07 -0700

    Add 5 rounds per game

--- javascript.js
+++ javascript.js
@@ -1,31 +1,32 @@
 function game() {
+    for (i= 0; i < 5; i++) {
+        const choiceOfWeapon = ["Rock", "Paper", "Scissors"];
 
-    const choiceOfWeapon = ["Rock", "Paper", "Scissors"];
-
-    function getComputerChoice() { 
-        return choiceOfWeapon[Math.floor(Math.random() * choiceOfWeapon.length)];
-    }
-
-    let playerWeapon = prompt("Choose your weapon... Rock, Paper, Scissors?");
-    let computerWeapon = getComputerChoice();
-
-    function playRound(playerWeapon, computerWeapon) {
-        playerWeapon = playerWeapon.toLowerCase();
-        computerWeapon = getComputerChoice().toLowerCase();
-        
-        if (playerWeapon == computerWeapon) {
-            return "Draw";
-        } else if ((playerWeapon == "rock" && computerWeapon == "paper") ||
-            (playerWeapon == "paper" && computerWeapon == "scissors") ||
-            (playerWeapon == "scissors" && computerWeapon == "rock"))
-            { return "Better luck next time, kid.";
-        } else if ((playerWeapon == "rock" && computerWeapon == "scissors") ||
-            (playerWeapon == "paper" && computerWeapon == "rock") ||
-            (playerWeapon == "scissors" && computerWeapon == "paper"))
-            { return "Huh, you won.";
-        } else (playerWeapon !== "rock" || "paper" || "scissors")
-            {return "Hey. Like they say, 'Don't bring a knife to a fist fight', bud.";
+        function getComputerChoice() { 
+            return choiceOfWeapon[Math.floor(Math.random() * choiceOfWeapon.length)];
         }
+
+        let playerWeapon = prompt("Choose your weapon... Rock, Paper, Scissors?");
+        let computerWeapon = getComputerChoice();
+
+            function playRound(playerWeapon, computerWeapon) {
+                playerWeapon = playerWeapon.toLowerCase();
+                computerWeapon = getComputerChoice().toLowerCase();
+                
+                if (playerWeapon == computerWeapon) {
+                    return "Draw";
+                } else if ((playerWeapon == "rock" && computerWeapon == "paper") ||
+                    (playerWeapon == "paper" && computerWeapon == "scissors") ||
+                    (playerWeapon == "scissors" && computerWeapon == "rock"))
+                    { return "Better luck next time, kid.";
+                } else if ((playerWeapon == "rock" && computerWeapon == "scissors") ||
+                    (playerWeapon == "paper" && computerWeapon == "rock") ||
+                    (playerWeapon == "scissors" && computerWeapon == "paper"))
+                    { return "Huh, you won.";
+                } else (playerWeapon !== "rock" || "paper" || "scissors")
+                    {return "Hey. Like they say, 'Don't bring a knife to a fist fight', bud.";
+                }
+            }
+            console.log(playRound(playerWeapon, computerWeapon));
     }
-    console.log(playRound(playerWeapon, computerWeapon));
 }
